077b9ec1fdd9befb199a0c4177212b0a50224eae
luungoc2005 <luungoc2005@gmail.com>
2019-04-01 10:52:21 +0800

    add webpack build settings

---
+++ sound_test_server/webpack.prod.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  externals: [nodeExternals()],
+  mode: 'production',
+  entry: {
+    app: './app.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env'
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
